8649c2b191bd18bce619f0b68102c2788b86c805
Justin Braaten <jstnbraaten@gmail.com>
2018-04-03 22:41:00 -0700

    finds dirs from head and processes multiple

--- 04_unpack_lt_ee_data_chunks.py
+++ 04_unpack_lt_ee_data_chunks.py
@@ -26,11 +26,20 @@ import ltcdb
 
 
 
-chunkDir = ltcdb.get_dir("Select the folder that contains LT-GEE files\n\n(*\\raster\\prep\\gee_chunks)")
-if chunkDir == '':
+headDir = ltcdb.get_dir("Select the project head folder")
+
+#headDir = r'D:\work\proj\al\gee_test\test'
+
+
+if headDir == '':
   sys.exit('ERROR: No folder containing LT-GEE files was selected.\nPlease re-run the script and select a folder.')
-  
-#TODO make sure  that there is gee_chunks in string
+
+chunkDir = os.path.join(headDir, 'raster', 'prep', 'gee_chunks')
+if not os.path.isdir(chunkDir):
+  sys.exit('ERROR: Can\'t find the gee_chunk folder.\nTrying to find it at this location: '+chunkDir+'\nIt\'s possible you provided an incorrect project head folder.\nPlease re-run the script and select the project head folder.')
+
+# could try to find the gee_chunk folder
+#[x[0] for x in os.walk(chunkDir)]
 
 
 # find the tif chunks
@@ -44,9 +53,13 @@ if len(tifs) == 0:
   sys.exit('ERROR: There are no TIF files in the folder selected.\nPlease fix this.')
 
 
-outDir = ltcdb.get_dir("Select folder to place LT segmentation outputs in\n\n(*\\raster\\landtrendr\segmentation)")
-if outDir == '':
-  sys.exit('ERROR: No folder containing LT-GEE files was selected.\nPlease re-run the script and select a folder.')
+outDir = os.path.normpath(os.path.join(chunkDir, os.sep.join([os.pardir]*2), 'landtrendr', 'segmentation'))
+# could just make the folder
+if not os.path.isdir(chunkDir):
+    sys.exit('ERROR: Can\'t find the segmentation folder.\nTrying to find it at this location: '+outDir+'\nIt\'s possible you have not run the directory setup script.\nPlease make sure the folder exists.')
+#outDir = ltcdb.get_dir("Select folder to place LT segmentation outputs in\n\n(*\\raster\\landtrendr\segmentation)")
+#if outDir == '':
+#  sys.exit('ERROR: No folder containing LT-GEE files was selected.\nPlease re-run the script and select a folder.')
   
 #TODO check for existing files
 
@@ -190,112 +203,112 @@ for name in names:
       os.remove(this)
 
 
-
-vertYrsFile = glob(os.path.join(thisOutDir,'*vert_yrs.tif'))[0]
-
-# TODO return error message if the vertYrsFile is not found
-
-
+  
+  vertYrsFile = glob(os.path.join(thisOutDir,'*vert_yrs.tif'))[0]
+  
+  # TODO return error message if the vertYrsFile is not found
 
 
-##########################################################################################################
-######## MAKE THE vert_fit_tc* FILES
-##########################################################################################################
-
- 
-# make a copy of vert files for tc vals
-vertTCBfitFile = vertYrsFile.replace('vert_yrs.tif', 'vert_fit_tcb.tif')
-vertTCGfitFile = vertYrsFile.replace('vert_yrs.tif', 'vert_fit_tcg.tif')
-vertTCWfitFile = vertYrsFile.replace('vert_yrs.tif', 'vert_fit_tcw.tif')
-outPuts = [vertTCBfitFile, vertTCGfitFile, vertTCWfitFile]
-
-ltcdb.make_output_blanks(vertYrsFile, outPuts, 0)
-
-ftvTCBfitFile = vertYrsFile.replace('vert_yrs.tif', 'ftv_tcb.tif')
-ftvTCGfitFile = vertYrsFile.replace('vert_yrs.tif', 'ftv_tcg.tif')
-ftvTCWfitFile = vertYrsFile.replace('vert_yrs.tif', 'ftv_tcw.tif')
-
-# open the output files for update
-dstTCB = gdal.Open(vertTCBfitFile, gdal.GA_Update)
-dstTCG = gdal.Open(vertTCGfitFile, gdal.GA_Update)
-dstTCW = gdal.Open(vertTCWfitFile, gdal.GA_Update)
-
-# open the vertYrs file for read
-srcYrs = gdal.Open(vertYrsFile, gdal.GA_ReadOnly)
-
-srcFtvTCB = gdal.Open(ftvTCBfitFile, gdal.GA_ReadOnly)
-srcFtvTCG = gdal.Open(ftvTCGfitFile, gdal.GA_ReadOnly)
-srcFtvTCW = gdal.Open(ftvTCWfitFile, gdal.GA_ReadOnly)
-
-##############################################################################
 
 
-# set some size variables
-ftvIndex =  ltcdb.year_to_band(os.path.basename(vertYrsFile))
-xSize = srcYrs.RasterXSize
-ySize = srcYrs.RasterYSize
-blockSize = 256
-
-##############################################################################
-
-for y in xrange(0, ySize, blockSize):
-  #yRange = range(0, ySize, blockSize)
-  #y=yRange[4]
-  if y + blockSize < ySize:
-    rows = blockSize
-  else:
-    rows = ySize - y
-  for x in xrange(0, xSize, blockSize):
-    #xRange = range(0, xSize, blockSize)
-    #x=xRange[4]
-    if x + blockSize < xSize:
-      cols = blockSize
+  ##########################################################################################################
+  ######## MAKE THE vert_fit_tc* FILES
+  ##########################################################################################################
+  
+   
+  # make a copy of vert files for tc vals
+  vertTCBfitFile = vertYrsFile.replace('vert_yrs.tif', 'vert_fit_tcb.tif')
+  vertTCGfitFile = vertYrsFile.replace('vert_yrs.tif', 'vert_fit_tcg.tif')
+  vertTCWfitFile = vertYrsFile.replace('vert_yrs.tif', 'vert_fit_tcw.tif')
+  outPuts = [vertTCBfitFile, vertTCGfitFile, vertTCWfitFile]
+  
+  ltcdb.make_output_blanks(vertYrsFile, outPuts, 0)
+  
+  ftvTCBfitFile = vertYrsFile.replace('vert_yrs.tif', 'ftv_tcb.tif')
+  ftvTCGfitFile = vertYrsFile.replace('vert_yrs.tif', 'ftv_tcg.tif')
+  ftvTCWfitFile = vertYrsFile.replace('vert_yrs.tif', 'ftv_tcw.tif')
+  
+  # open the output files for update
+  dstTCB = gdal.Open(vertTCBfitFile, gdal.GA_Update)
+  dstTCG = gdal.Open(vertTCGfitFile, gdal.GA_Update)
+  dstTCW = gdal.Open(vertTCWfitFile, gdal.GA_Update)
+  
+  # open the vertYrs file for read
+  srcYrs = gdal.Open(vertYrsFile, gdal.GA_ReadOnly)
+  
+  srcFtvTCB = gdal.Open(ftvTCBfitFile, gdal.GA_ReadOnly)
+  srcFtvTCG = gdal.Open(ftvTCGfitFile, gdal.GA_ReadOnly)
+  srcFtvTCW = gdal.Open(ftvTCWfitFile, gdal.GA_ReadOnly)
+  
+  ##############################################################################
+  
+  
+  # set some size variables
+  ftvIndex =  ltcdb.year_to_band(os.path.basename(vertYrsFile))
+  xSize = srcYrs.RasterXSize
+  ySize = srcYrs.RasterYSize
+  blockSize = 256
+  
+  ##############################################################################
+  
+  for y in xrange(0, ySize, blockSize):
+    #yRange = range(0, ySize, blockSize)
+    #y=yRange[4]
+    if y + blockSize < ySize:
+      rows = blockSize
     else:
-      cols = xSize - x
-    
-    npYrs = srcYrs.ReadAsArray(x, y, cols, rows)
-       
-    npOutTCB = dstTCB.ReadAsArray(x, y, cols, rows)
-    npOutTCG = dstTCG.ReadAsArray(x, y, cols, rows)
-    npOutTCW = dstTCW.ReadAsArray(x, y, cols, rows)
-    
-    npFtvTCB = srcFtvTCB.ReadAsArray(x, y, cols, rows)
-    npFtvTCG = srcFtvTCG.ReadAsArray(x, y, cols, rows)
-    npFtvTCW = srcFtvTCW.ReadAsArray(x, y, cols, rows)
-    
-    nVerts, subYsize, subXsize = npYrs.shape
-    for subY in xrange(subYsize):
-      #subY = 0
-      #progress = (y+1.0)/ySize
-      #update_progress(progress)    
-      for subX in xrange(subXsize):
-        #subX = 0
-        vertYrsPix = npYrs[:, subY, subX]
-        
-        # check to see if this is a NoData pixel    
-        if (vertYrsPix == 0).all():
-          continue
-        
-        vertIndex = vertYrsPix[np.where(vertYrsPix != 0)[0]]
-        theseBands = ftvIndex[vertIndex]
-        replaceThese = range(len(theseBands))
-        npOutTCB[replaceThese, subY, subX] = npFtvTCB[theseBands, subY, subX]
-        npOutTCG[replaceThese, subY, subX] = npFtvTCG[theseBands, subY, subX]
-        npOutTCW[replaceThese, subY, subX] = npFtvTCW[theseBands, subY, subX]
-        
-      for b in range(nVerts):
-        ltcdb.write_array(dstTCB, b, npOutTCB, x, y)
-        ltcdb.write_array(dstTCG, b, npOutTCG, x, y)
-        ltcdb.write_array(dstTCW, b, npOutTCW, x, y)
+      rows = ySize - y
+    for x in xrange(0, xSize, blockSize):
+      #xRange = range(0, xSize, blockSize)
+      #x=xRange[4]
+      if x + blockSize < xSize:
+        cols = blockSize
+      else:
+        cols = xSize - x
       
-# close the output files
-dstTCB = None
-dstTCG = None
-dstTCW = None
-srcYrs = None
-srcFtvTCB = None
-srcFtvTCG = None
-srcFtvTCW = None
+      npYrs = srcYrs.ReadAsArray(x, y, cols, rows)
+         
+      npOutTCB = dstTCB.ReadAsArray(x, y, cols, rows)
+      npOutTCG = dstTCG.ReadAsArray(x, y, cols, rows)
+      npOutTCW = dstTCW.ReadAsArray(x, y, cols, rows)
+      
+      npFtvTCB = srcFtvTCB.ReadAsArray(x, y, cols, rows)
+      npFtvTCG = srcFtvTCG.ReadAsArray(x, y, cols, rows)
+      npFtvTCW = srcFtvTCW.ReadAsArray(x, y, cols, rows)
+      
+      nVerts, subYsize, subXsize = npYrs.shape
+      for subY in xrange(subYsize):
+        #subY = 0
+        #progress = (y+1.0)/ySize
+        #update_progress(progress)    
+        for subX in xrange(subXsize):
+          #subX = 0
+          vertYrsPix = npYrs[:, subY, subX]
+          
+          # check to see if this is a NoData pixel    
+          if (vertYrsPix == 0).all():
+            continue
+          
+          vertIndex = vertYrsPix[np.where(vertYrsPix != 0)[0]]
+          theseBands = ftvIndex[vertIndex]
+          replaceThese = range(len(theseBands))
+          npOutTCB[replaceThese, subY, subX] = npFtvTCB[theseBands, subY, subX]
+          npOutTCG[replaceThese, subY, subX] = npFtvTCG[theseBands, subY, subX]
+          npOutTCW[replaceThese, subY, subX] = npFtvTCW[theseBands, subY, subX]
+          
+        for b in range(nVerts):
+          ltcdb.write_array(dstTCB, b, npOutTCB, x, y)
+          ltcdb.write_array(dstTCG, b, npOutTCG, x, y)
+          ltcdb.write_array(dstTCW, b, npOutTCW, x, y)
+        
+  # close the output files
+  dstTCB = None
+  dstTCG = None
+  dstTCW = None
+  srcYrs = None
+  srcFtvTCB = None
+  srcFtvTCG = None
+  srcFtvTCW = None
 
 
 
