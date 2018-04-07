39ec2465f62f56b4ed21b6c01db1d45ff9551846
Justin Braaten <jstnbraaten@gmail.com>
2018-04-07 14:04:29 -0700

    Revert "Revert "added new dependent libraries and reduced code redundancy""
    This reverts commit adc627db8359fe586fbe3e8199cfddf96092745b.

--- 01_dependency_check.py
+++ 01_dependency_check.py
@@ -9,66 +9,66 @@ import imp
 import subprocess
 
 
+# =============================================================================
+# 	Define the dependent libraries
+# =============================================================================
+
+libs = [
+  'pandas',
+  'rasterstats',
+  'rasterio',
+  'shapely',
+  'fiona',
+  'shapely',
+  'gdal'
+]
+
+
+
 
 # =============================================================================
 # 	Verify we have conda-forge channel
 # =============================================================================
+
 cmd = 'conda config --add channels conda-forge'
 subprocess.call(cmd, shell=True)
 
 
 
+
 # =============================================================================
-# 	Verify we have GDAL
+# 	Verify we have all the dependent libraries.
 # =============================================================================
-found = False
-while (found == False):
-  try:
-    print('\nChecking to see if GDAL is installed...\n')
-    blank = imp.find_module('gdal')
-    found = True
-  except ImportError:
-    print('GDAL is not installed, starting install...\n')
-    cmd = 'conda install -c conda-forge gdal'
-    subprocess.call(cmd, shell=True)
-    try:     
-      blank = imp.find_module('gdal')
+
+for lib in libs:
+  found = False
+  while (found == False):
+    try:
+      print('\nChecking to see if '+lib+' is installed...\n')
+      blank = imp.find_module(lib)
       found = True
     except ImportError:
-      print('\nIt looks like you didn\'t choose to install it, try again\n')
-      found = False
+      print(lib+' is not installed, starting install...\n')
+      cmd = 'conda install -c conda-forge '+lib
+      subprocess.call(cmd, shell=True)
+      try:     
+        blank = imp.find_module(lib)
+        found = True
+      except ImportError:
+        print('\nIt looks like you didn\'t choose to install it, try again\n')
+        found = False
 
-
-    
-# =============================================================================
-# 	Verify we have NumPy
-# =============================================================================  
-found = False
-while (found == False):   
-  try:
-    print('\nChecking to see if NumPy is installed...\n')
-    blank = imp.find_module('numpy')
-    found = True
-  except ImportError:
-    print('NumPy is not installed, starting install...\n')
-    cmd = 'conda install -c conda-forge numpy'
-    subprocess.call(cmd, shell=True)
-    try:     
-      blank = imp.find_module('numpy')
-      found = True
-    except ImportError:
-      print('\nIt looks like you didn\'t choose to install it, try again\n')
-      found = False
 
 
 
 # import GDAL to check on bindings for some methods
 from osgeo import gdal
-
 error = 0
+
 # =============================================================================
 # 	Verify we have next gen bindings with the sievefilter method.
 # =============================================================================
+
 try:
   print('Checking to see if gdal.SieveFilter() is available...\n')
   blank = gdal.SieveFilter
@@ -82,6 +82,7 @@ except:
 # =============================================================================
 # 	Verify we have next gen bindings with the polygonize method.
 # =============================================================================
+
 try:
   print('Checking to see if gdal.Polygonize() is available...\n')
   blank = gdal.Polygonize
