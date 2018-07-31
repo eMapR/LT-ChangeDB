7b5061e94f16336912ff8ac6542cba7ef7abcaf6
Justin Braaten <jstnbraaten@gmail.com>
2018-07-30 17:33:55 -0700

    moved to archive

--- archive/misc_make_centroids.py
+++ archive/misc_make_centroids.py
@@ -25,9 +25,9 @@ if os.path.splitext(inFile)[1] != '.shp':
 
 outFile = ltcdb.save_file('Save Centroid File As:')
 
-inFile = r"D:\work\proj\park_service\nccn-glkn\LT-ChangeDB_test\mora\vector\change\PARK_CODE-MORA-NBRz-7-19842017-06010930-dist_info_11mmu_8con\PARK_CODE-MORA-NBRz-7-19842017-06010930-dist_info_11mmu_8con_1992.shp"
-inFile = r"D:\work\proj\park_service\nccn-glkn\LT-ChangeDB_test\mora\vector\test\dist_info_1992.shp"
-outFile = r"D:\work\temp\___righthere11111.shp"
+#inFile = r"D:\work\proj\park_service\nccn-glkn\LT-ChangeDB_test\mora\vector\change\PARK_CODE-MORA-NBRz-7-19842017-06010930-dist_info_11mmu_8con\PARK_CODE-MORA-NBRz-7-19842017-06010930-dist_info_11mmu_8con_1992.shp"
+#inFile = r"D:\work\proj\park_service\nccn-glkn\LT-ChangeDB_test\mora\vector\test\dist_info_1992.shp"
+#outFile = r"D:\work\temp\___righthere11111.shp"
 
 tableName = ('\"'+os.path.splitext(os.path.basename(inFile))[0]+'\"')
 tableName = os.path.splitext(os.path.basename(inFile))[0]
