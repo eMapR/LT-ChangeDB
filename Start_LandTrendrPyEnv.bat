e4275da57207c5b8088e8ac917b22573e77bb872
Justin Braaten <jstnbraaten@gmail.com>
2018-12-18 14:36:35 -0800

    getting return right

--- Start_LandTrendrPyEnv.bat
+++ Start_LandTrendrPyEnv.bat
@@ -1,9 +1,5 @@
 SET LT_PY_ENV=C:\mock\LandTrendrPyEnv
-
 SET PATH=%LT_PY_ENV%;%LT_PY_ENV%\Library\bin;%LT_PY_ENV%\Scripts;%LT_PY_ENV%\site-packages;%~dp0
-
 SET GDAL_DATA=%LT_PY_ENV%\Library\share\gdal
-
 SET PYTHONPATH=%LT_PY_ENV%\site-packages
-
 start cmd.exe /k
